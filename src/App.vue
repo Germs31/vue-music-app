20387e1b36198bd4c88bf8885f15153a64e2e4d3
german nunez <german90nunez@gmail.com>
2020-10-07 19:00:29 -0700

    basic styling

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,8 @@
     </header>
     <main>
       <section class="player">
-        <h2 class="song-title">{{ current.title}}</h2>
-        <span>{{current.artist}}</span>
+        <h2 class="song-title">{{ current.title}} - <span>{{current.artist}}</span></h2>
+        
         <div class="controls">
           <button class="prev"
                   @click="prev">Prev</button>
@@ -65,7 +65,17 @@ export default {
         this.current = song;
         this.player.src = this.current.src;
       }
+
       this.player.play();
+      this.player.addEventListener('ended', function () {
+        this.index++
+        if(this.index > this.songs.length -1) {
+          this.index = 0;
+        }
+        this.current = this.songs[this.index];
+        this.play(this.current);
+      }.bind(this))
+
       this.isPlaying = true;
     },
     pause() {
@@ -114,5 +124,93 @@ header {
   padding: 15px;
   background: #212121;
   color: #fff;
+}
+
+main {
+  width: 100%;
+  max-width: 768px;
+  margin: 0 auto;
+  padding: 25px
+}
+
+.song-title{
+  color: #212121;
+  font-size: 32px;
+  font-weight: 700;
+  text-transform: uppercase;
+  text-align: center;
+}
+
+.song-title span {
+  font-weight: 400;
+  font-style: italic;
+}
+
+.controls {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 30px 15px;
+}
+
+button {
+  appearance: none;
+  background: none;
+  border: none;
+  cursor: pointer;
+}
+
+button:hover {
+  opacity: 0.8;
+}
+
+.play, .pause {
+  font-size: 20px;
+  font-weight: 700px;
+  padding: 15px 25px;
+  margin: 0 15px;
+  border-radius: 8px;
+  background: #cc2e5d;
+  color: #fff;
+}
+
+.next, .prev {
+  font-size: 16px;
+  font-weight: 700px;
+  padding: 10px 20px;
+  margin: 0 15px;
+  border-radius: 6px;
+  background: #cc2e5d; 
+  color:#fff;
+}
+
+.playlist {
+  padding: 0 30px;
+}
+
+.playlist h3 {
+  color: #212121;
+  font-size: 28px;
+  font-weight: 400;
+  margin-bottom: 30px;
+  text-align: center;
+}
+
+.playlist .song {
+  display: block;
+  width: 100%;
+  padding: 15px;
+  font-size: 20px;
+  font-weight: 700;
+  cursor: pointer;
+}
+
+.playlist .song:hover {
+  color: #ff5858;
+}
+
+.playlist .song.playing{
+  color: #fff;
+  background-image: linear-gradient(to right, #cc2e5d, #ff5858);
 }
 </style>
